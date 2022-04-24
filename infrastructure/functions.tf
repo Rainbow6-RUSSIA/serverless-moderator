70b12841999b8254b8f09cfebf89e5012773fcb0
BadCoder1337 <anton.tiz.musin@gmail.com>
2022-04-24 04:48:56 +0300

    Fix entrypoint

--- infrastructure/functions.tf
+++ infrastructure/functions.tf
@@ -10,7 +10,7 @@ resource "yandex_function" "test_function" {
   description        = "serverless bot entrypoint"
   user_hash          = substr(data.git_repository.info.commit_sha, 0, 7)
   runtime            = "nodejs16"
-  entrypoint         = "handler.index"
+  entrypoint         = "index.handler"
   memory             = "128"
   execution_timeout  = "30"
   service_account_id = data.yandex_iam_service_account.deployer.id
