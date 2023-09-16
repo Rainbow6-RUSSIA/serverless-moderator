d7aaa5d139175ee5161d3f160e8fae7513a85499
BadCoder1337 <fazebook@rainbow6.ru>
2023-09-16 22:56:38 +0300

    fix serverless client

--- src/util/env.ts
+++ src/util/env.ts
@@ -1,18 +1,27 @@
 import assert from "assert";
 import { config } from "dotenv";
+import { webcrypto } from "crypto";
 
-config()
+config();
 
 export const isDev = process.env.NODE_ENV === "development";
 
 export const discordPublicKey = process.env.DISCORD_PUBLIC_KEY;
+export const discordPublicKeyObject = await webcrypto.subtle.importKey(
+  "raw",
+  Buffer.from(discordPublicKey, "hex"),
+  { name: "Ed25519" },
+  true,
+  ["verify"]
+);
+export const discordToken = process.env.DISCORD_TOKEN;
 
 export const accessKeyId = process.env.DB_ACCESS_KEY_ID;
 export const secretAccessKey = process.env.DB_SECRET_ACCESS_KEY;
 export const region = process.env.DB_REGION ?? "ru-central1";
 export const endpoint = process.env.DB_ENDPOINT;
 
-assert(discordPublicKey, "No discordPublicKey")
+assert(discordPublicKey, "No discordPublicKey");
 // assert(accessKeyId, "No accessKeyId")
 // assert(secretAccessKey, "No secretAccessKey")
 // assert(region, "No region")
--- src/util/serverless-client.ts
+++ src/util/serverless-client.ts
@@ -1,17 +1,30 @@
 import { Store } from "@sapphire/pieces";
 import { Client, ListenOptions } from "@skyra/http-framework";
-import { FastifyInstance } from "fastify";
-import { isDev } from "./env.js";
+import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
+import { discordPublicKeyObject, isDev } from "./env.js";
 
 if (isDev) Store.logger = console.log;
 
 export class ServerlessClient extends Client {
-    async listen(options: ListenOptions) {
-        throw new Error("Unsupported in serverless environment");
-    }
+  async listen(_: ListenOptions) {
+    throw new Error("Unsupported in serverless environment");
+  }
 
-    async plugin(fastify: FastifyInstance) {
-        fastify.post("/", this.handleHttpMessage.bind(this));
-        this.server = fastify.server;
-    }
+  async handle(req: FastifyRequest, res: FastifyReply) {
+    return this.handleRawHttpMessage(
+      Object.assign(req.raw, {
+        async *[Symbol.asyncIterator]() {
+          yield JSON.stringify(req.body);
+        },
+      }),
+      res.raw,
+      req.url,
+      discordPublicKeyObject
+    );
+  }
+
+  async plugin(fastify: FastifyInstance) {
+    fastify.post("/", this.handle.bind(this));
+    this.server = fastify.server;
+  }
 }
